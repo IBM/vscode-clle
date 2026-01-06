2678865ecb89efcb1f7aa62e42b4c1cf1243ad56
Shreya Singh <shreya.singh9@ibm.com>
2026-01-06 17:13:06 +0530

    registering command for get cl definition

--- client/src/commands.ts
+++ client/src/commands.ts
@@ -1,11 +1,20 @@
 import { ExtensionContext, commands, Uri } from 'vscode';
 import { LanguageClient } from 'vscode-languageclient';
 import { getModules } from './requests';
+import { getHandler } from "./external";
 
 export function registerCommands(context: ExtensionContext, client: LanguageClient) {
   context.subscriptions.push(
     commands.registerCommand(`vscode-clle.server.getModules`, async (uri: Uri, content: string) => {
       return await getModules(client, uri, content);
+    }),
+    commands.registerCommand(`vscode-clle.getCLDefinition`, async (object: string, library?: string) => {
+          const handler = await getHandler();
+          if (!handler) {
+            throw new Error("IBM i handler not available");
+          }
+          const def = library ? await handler.getCLDefinition(object, library) : await handler.getCLDefinition('CPYF');
+          return def;
     })
   )
 }
